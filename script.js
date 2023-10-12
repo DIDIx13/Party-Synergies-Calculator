1255b249891dbc33dab628ea5b843a5db5aad47d
Darwin Ameli <darwin.ameli@rpn.ch>
2023-10-12 02:03:02 +0200

    Ajout du Last Updated

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Fetch JSON data from data.json file
-    fetch("data.json")
+
+    // Update Last Updated
+    const lastUpdated = document.getElementById('lastUpdated');
+    lastUpdated.textContent = getFormattedDate();
+
+    fetchClassData()
+        .then(data => createCheckboxes(data))
+        .catch(error => console.error('Fetch error: ', error));
+
+});
+
+function getFormattedDate() {
+    // Utilisez la date actuelle ou remplacez par la date de la dernière mise à jour
+    const date = new Date();
+
+    // Tableau des noms des mois
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+    // Récupérez le nom du mois
+    const monthName = monthNames[date.getMonth()];
+
+    // Formatez la date comme vous le souhaitez
+    return `${date.getDate()} ${monthName} ${date.getFullYear()}`;
+}
+
+function fetchClassData() {
+    return fetch("data.json")
         .then(response => {
-            // Check if the response is OK (status 200)
             if (!response.ok) {
                 throw new Error('Network response was not ok' + response.statusText);
             }
-            return response.json();  // Parse JSON data
-        })
-        .then(data => {
-            // Get the form element where the checkboxes will be appended
-            const classForm = document.getElementById('classForm');
-            const ulElement = document.createElement('ul');
-        
-            // Iterate through JSON data and create checkboxes
-            data.forEach(classData => {
-                const liElement = document.createElement('li');
-                const labelElement = document.createElement('label');
-                const checkboxElement = document.createElement('input');
-            
-                checkboxElement.type = 'checkbox';
-                checkboxElement.className = 'classCheckbox';
-                checkboxElement.value = classData.class;
-            
-                labelElement.appendChild(checkboxElement);
-                labelElement.appendChild(document.createTextNode(classData.class));
-            
-                liElement.appendChild(labelElement);
-                ulElement.appendChild(liElement);
-            });
-        
-            classForm.appendChild(ulElement);
-        })
-        .catch(error => {
-            console.error('Fetch error: ', error);
+            return response.json();
         });
+}
 
-    // Add additional JavaScript here, such as event listeners for form submission
-});
+function createCheckboxes(data) {
+    const classForm = document.getElementById('classForm');
+    const ulElement = document.createElement('ul');
+
+    data.forEach(classData => {
+        const liElement = createCheckboxElement(classData);
+        ulElement.appendChild(liElement);
+    });
+
+    classForm.appendChild(ulElement);
+}
+
+function createCheckboxElement(classData) {
+    const liElement = document.createElement('li');
+    const labelElement = document.createElement('label');
+    const checkboxElement = document.createElement('input');
+
+    checkboxElement.type = 'checkbox';
+    checkboxElement.className = 'classCheckbox';
+    checkboxElement.value = classData.class;
+
+    labelElement.appendChild(checkboxElement);
+    labelElement.appendChild(document.createTextNode(classData.class));
+
+    liElement.appendChild(labelElement);
+
+    return liElement;
+}
